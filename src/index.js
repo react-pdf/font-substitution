f0418e7724c1d3cbc816db4adce8761d9c085447
Diego Muracciole <diegomuracciole@gmail.com>
2018-04-17 13:41:49 -0300

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,48 @@
+import { Run } from 'textkit';
+import StandardFont from './standardFont';
+
+export default class FontSubstitutionEngine {
+  getRuns(string, runs) {
+      const res = [];
+      let lastFont = null;
+      let lastIndex = 0;
+      let index = 0;
+
+      for (const run of runs) {
+        let defaultFont;
+
+        if (typeof run.attributes.font === 'string') {
+          defaultFont = new StandardFont(run.attributes.font);
+        } else {
+          defaultFont = run.attributes.font;
+        }
+
+        for (const char of string.slice(run.start, run.end)) {
+          let font = null;
+
+          font = defaultFont;
+
+          if (font !== lastFont) {
+            if (lastFont) {
+              res.push(
+                new Run(lastIndex, index, { font: lastFont })
+              );
+            }
+
+            lastFont = font;
+            lastIndex = index;
+          }
+
+          index += char.length;
+        }
+      }
+
+      if (lastIndex < string.length) {
+        res.push(
+          new Run(lastIndex, string.length, { font: lastFont })
+        );
+      }
+
+      return res;
+  }
+}
---
+++ src/standardFont.js
@@ -0,0 +1,44 @@
+import PDFFont from 'pdfkit/js/font';
+
+class StandardFont {
+  constructor(src) {
+    this.name = src;
+    this.src = PDFFont.open(null, src);
+  }
+
+  layout(str) {
+    const [encoded, positions] = this.src.encode(str);
+
+    return {
+      positions,
+      stringIndices: positions.map((_, i) => i),
+      glyphs: encoded.map(g => ({ id: g, _font: this.src })),
+    }
+  }
+
+  glyphForCodePoint(codePoint) {
+    return this.getGlyph({ id:codePoint, _font: this.src, advanceWidth: 400 });
+  }
+
+  getGlyph(glyph) {
+    return glyph;
+  }
+
+  get ascent() {
+    return this.src.ascender;
+  }
+
+  get descent() {
+    return -this.src.descender;
+  }
+
+  get lineGap() {
+    return this.src.lineGap;
+  }
+
+  get unitsPerEm() {
+    return 1000;
+  }
+}
+
+export default StandardFont;
