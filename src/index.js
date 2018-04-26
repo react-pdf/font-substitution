efe8bee7ee257012eab38e60994c211f1e67736c
Diego Muracciole <diegomuracciole@gmail.com>
2018-04-26 12:29:13 -0300

    Return run for empty glyphString

--- src/index.js
+++ src/index.js
@@ -1,49 +1,48 @@
 import StandardFont from './standardFont';
 
-export default ({ Run }) => (
-  class FontSubstitutionEngine {
-    getRuns(string, runs) {
-        const res = [];
-        let lastFont = null;
-        let lastIndex = 0;
-        let index = 0;
+export default({Run}) => (class FontSubstitutionEngine {
+  getRuns(string, runs) {
+    const res = [];
+    let lastFont = null;
+    let lastIndex = 0;
+    let index = 0;
 
-        for (const run of runs) {
-          let defaultFont;
+    for (const run of runs) {
+      let defaultFont;
 
-          if (typeof run.attributes.font === 'string') {
-            defaultFont = new StandardFont(run.attributes.font);
-          } else {
-            defaultFont = run.attributes.font;
+      if (typeof run.attributes.font === 'string') {
+        defaultFont = new StandardFont(run.attributes.font);
+      } else {
+        defaultFont = run.attributes.font;
+      }
+
+      if (string.length === 0) {
+        res.push(new Run(0, 0, { font: defaultFont }));
+        break;
+      }
+
+      for (const char of string.slice(run.start, run.end)) {
+        let font = null;
+
+        font = defaultFont;
+
+        if (font !== lastFont) {
+          if (lastFont) {
+            res.push(new Run(lastIndex, index, {font: lastFont}));
           }
 
-          for (const char of string.slice(run.start, run.end)) {
-            let font = null;
-
-            font = defaultFont;
-
-            if (font !== lastFont) {
-              if (lastFont) {
-                res.push(
-                  new Run(lastIndex, index, { font: lastFont })
-                );
-              }
-
-              lastFont = font;
-              lastIndex = index;
-            }
-
-            index += char.length;
-          }
+          lastFont = font;
+          lastIndex = index;
         }
 
-        if (lastIndex < string.length) {
-          res.push(
-            new Run(lastIndex, string.length, { font: lastFont })
-          );
-        }
-
-        return res;
+        index += char.length;
+      }
     }
+
+    if (lastIndex < string.length) {
+      res.push(new Run(lastIndex, string.length, {font: lastFont}));
+    }
+
+    return res;
   }
-)
+})
